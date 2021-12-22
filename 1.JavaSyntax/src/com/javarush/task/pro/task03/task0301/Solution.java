19d8603a23cfd9e39d0a4f468387f1b7659849dd
stepawka62 <Stepawka6218@gmail.com>
2021-12-22 17:51:05 +0300

    Add solution for JavaSyntax task 0301

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0301/Solution.java
@@ -10,6 +10,13 @@ public class Solution {
     public static void main(String[] args) {
         String cold = "на улице холодно";
         String warm = "на улице тепло";
-        //напишите тут ваш код
+
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        if (t < 0) {
+            System.out.print(cold);
+        } else {
+            System.out.print(warm);
+        }
     }
 }

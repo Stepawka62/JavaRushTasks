6ea192b20a856bd6f27bb3488fb333a6d3c4b310
stepawka62 <Stepawka6218@gmail.com>
2021-12-29 14:37:29 +0300

    Add solution for JavaSyntax task 0407

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0407/Solution.java
@@ -10,14 +10,10 @@ public class Solution {
         int i = 0;
        while(i < 100) {
                 i++;
-
-           if (i % 3 == 0) {
-
+            if (i % 3 == 0) {
                continue;
            }
            sum = sum + i;
-
-
        }
         System.out.println(sum);
     }

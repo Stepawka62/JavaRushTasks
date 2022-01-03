64969b4b0f5edd9a7f620e78aac2f4a8356f5dcd
stepawka62 <Stepawka6218@gmail.com>
2022-01-03 17:03:04 +0300

    Add solution for JavaSyntax task 0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double sum = 1.0 * a / b;
+        System.out.println(sum);
+
+    }
+}
